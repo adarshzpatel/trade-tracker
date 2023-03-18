acf9acc7ec9ad9c1646e827366dd06f013b7d6fe
adarshzpatel <adarshzpatel@gmail.com>
2023-03-18 07:32:51 +0530

    added risk per trade

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -30,24 +30,28 @@ const Stats = (props: Props) => {
   const usdtToInr = 89
   return (
     <Grid className='mt-4 gap-4' >
+      <Card>
+        <Text>Risk Per Trade</Text>
+        <Metric>${(data[data.length-1]?.currentBalance * 0.01).toFixed()}</Metric>
+      </Card>
        <Card>
         <Text>Net Change</Text>
         <Metric>${netProfit.toFixed(2)}</Metric>
       </Card>
-      <Card>
+      <Card decoration='top' decorationColor='green'>
         <Text>Total Profit</Text>
         <Metric>${(profit - data[0]?.change).toFixed(2)} </Metric>
       </Card>
-      <Card>
-        <Text>Total Loss</Text>
-        <Metric>{loss.toFixed(2)}</Metric>
-      </Card>
-      <Card>
+      <Card decoration='top' decorationColor='green'>
         <Text>Avg. Profit</Text>
         <Metric>${avgProfit}</Metric>
       </Card>
-      <Card>
+      <Card decoration='top' decorationColor='red'>
         <Text>Total Loss</Text>
+        <Metric>{loss.toFixed(2)}</Metric>
+      </Card>
+      <Card decoration='top' decorationColor='red'>
+        <Text>Average Loss</Text>
         <Metric>${avgLoss}</Metric>
       </Card>
     </Grid>
